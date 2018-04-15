3ab7ad5453622047f180f44f76015585c837115a
nromike <nromike@newrelic.com>
2018-04-15 02:37:28 -0700

    Read response and send requests for id queries

--- mock_processor.rb
+++ mock_processor.rb
@@ -1,25 +1,49 @@
 require 'json'
 
 class MockProcessor
+  # Constants
+  API_URL = 'http://localhost:9292/'
+  HTTP_METHOD = 'GET'
+  SILENT_OUTPUT = '2>/dev/null'
 
-  def self.create_api_request(params, path)
-    http_method = 'GET'
-    uri = 'http://localhost:9292/'
+  @output_file = File.new('processed_output.json', 'w')
+
+  def self.make_api_request(path, params)
     headers = "-H 'x-api-key: hello'"
-    silence = '2>/dev/null'
-    `curl -X #{http_method} #{uri}#{path}#{params} #{headers} #{silence}`
+    `curl -X #{HTTP_METHOD} #{API_URL}#{path}#{params} #{headers} #{SILENT_OUTPUT}`
   end
 
   def self.save_to_file(json)
-    out_file = File.new('processed_output.json', 'w')
-    out_file.puts(json)
-    out_file.close
+    @output_file.puts(json)
+  end
+
+  def self.read_response(path, response)
+    response_hash = JSON.parse(response)
+
+    # The "results" array is unique to requests for IDs
+    id_array = response_hash['results']
+    if id_array
+      total_pages = response_hash['pages']
+      current_page = response_hash['page']
+      puts "Total number of pages: #{total_pages}, Currently on page ##{current_page}"
+
+      id_array = id_array.sort
+      id_array.each do |id|
+        puts "Processing request for ID: #{id}"
+        save_to_file(make_api_request("#{path}/#{id}", '?page=1'))
+      end
+
+    else
+      individual_name = response_hash['name']
+      puts "Hello #{individual_name}"
+    end
   end
 
   path = 'orgs'
   params = '?page=1'
-  response = create_api_request(params, path)
+  response = make_api_request(path, params)
 
   save_to_file(response)
-
+  read_response(path, response)
+  @output_file.close
 end
