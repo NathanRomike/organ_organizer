d718b8a09579b0537c4d8d7d80e9e9f43bfe1dcd
nromike <nromike@newrelic.com>
2018-04-14 23:57:53 -0700

    Make request to /orgs endpoint

--- mock_processor.rb
+++ mock_processor.rb
@@ -1,3 +1,17 @@
+require 'json'
+
 class MockProcessor
 
+  def self.create_api_request(params, path)
+    http_method = 'GET'
+    uri = 'http://localhost:9292/'
+    headers = "-H 'x-api-key: hello'"
+    silence = '2>/dev/null'
+    `curl -X #{http_method} #{uri}#{path}#{params} #{headers} #{silence}`
+  end
+
+  path = 'orgs'
+  params = '?page=2'
+  response = create_api_request(params, path)
+  puts JSON.parse(response)
 end
