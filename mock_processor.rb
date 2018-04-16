9375625bbff1d4e018ca4ec775e46626bae2f3cc
nromike <nromike@newrelic.com>
2018-04-15 20:17:14 -0700

    Correct id array issue, improve comments

--- mock_processor.rb
+++ mock_processor.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'pry'
 
 class MockProcessor
   # Constants
@@ -17,7 +18,7 @@ class MockProcessor
 
   def self.make_api_request(path, params = '')
     response = `curl -X #{GET_METHOD} #{API_URL}#{path}#{params} #{API_KEY_HEADER} #{SILENT_OUTPUT} #{WRITE_STATUS_CODE}`
-    response_status_code = response[-3..-1] # at last 3 characters of response
+    response_status_code = response[-3..-1] # status code at last 3 characters of response
     case response_status_code
     when '503' # request was unavailable - retry immediately
       make_api_request(path, params)
@@ -25,13 +26,13 @@ class MockProcessor
       sleep(30)
       make_api_request(path, params)
     when '200' # OK!
-      return response[0...-3]
+      return response[0...-3] # response minus status code
     end
   end
 
-  def self.save_to_file(json)
+  def self.save_to_file(string_to_store)
     # TODO: maybe add flag parameter(s) to help with formatting - like HEAD_NODE
-    @output_file.puts("#{json}")
+    @output_file.puts("#{string_to_store}")
   end
 
   def self.request_all_ids(endpoint)
@@ -45,15 +46,25 @@ class MockProcessor
     # loops through and request all remaining pages
     while current_page < total_pages
       current_page = current_page + 1
-      all_ids_array << JSON.parse(make_api_request(endpoint, PAGE_PARAM + current_page.to_s))['results']
+      (all_ids_array << JSON.parse(make_api_request(endpoint, PAGE_PARAM + current_page.to_s))['results']).flatten!
     end
-
     return all_ids_array
   end
 
-  save_to_file('{"' + ORGANS_ENDPOINT + '":[' + request_all_ids(ORGANS_ENDPOINT).to_s.delete('[]') + ']},')
-  save_to_file('{"' + USERS_ENDPOINT + '":[' + request_all_ids(USERS_ENDPOINT).to_s.delete('[]') + ']},')
-  save_to_file('{"' + ACCOUNTS_ENDPOINT + '":[' + request_all_ids(ACCOUNTS_ENDPOINT).to_s.delete('[]') + ']},')
+  def self.request_all_organs(id_list)
+    responses = []
+    id_list.each do |id|
+      organ_response = make_api_request(ORGANS_ENDPOINT + "/#{id}")
+      puts organ_response
+      responses << organ_response
+    end
+    return responses
+  end
+
+  list_of_ids = request_all_ids(ORGANS_ENDPOINT)
+  all_organs = request_all_organs(list_of_ids)
+  all_organs.find_all { |organ| organ['parent_id'].nil? }
+  save_to_file(all_organs)
 
   @output_file.close
 end
