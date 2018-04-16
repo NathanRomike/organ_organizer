e0ddb61be211c3234384b617b2f9e3ce33d02fc4
nromike <nromike@newrelic.com>
2018-04-15 18:05:32 -0700

    Add error response handling and request retrying

--- mock_processor.rb
+++ mock_processor.rb
@@ -3,9 +3,11 @@ require 'json'
 class MockProcessor
   # Constants
   API_URL = 'http://localhost:9292/'
-  HTTP_METHOD = 'GET'
+  GET_METHOD = 'GET'
   SILENT_OUTPUT = '--silent'
   PAGE_PARAM = '?page='
+  API_KEY_HEADER = "-H 'x-api-key: hello'"
+  WRITE_STATUS_CODE = '-w "%{http_code}"'
 
   ORGANS_ENDPOINT = 'orgs'
   ACCOUNTS_ENDPOINT = 'accounts'
@@ -14,20 +16,17 @@ class MockProcessor
   @output_file = File.new('processed_output.json', 'w')
 
   def self.make_api_request(path, params = '')
-    headers = "-H 'x-api-key: hello'"
-    status_flag = '-w "%{http_code}"'
-    response = `curl -X #{HTTP_METHOD} #{API_URL}#{path}#{params} #{headers} #{SILENT_OUTPUT} #{status_flag}`
-
-    response_code = response[-3..-1]
-
-    # TODO: figure out a better way to avoid rate limit and 503s
-    # I'm thinking instant retries for 503s and minute delay after rate limit
-    # if (status_code != 200) {sleep for a minute - then make_api_request with same params}
-    sleep(0.3)
-    # status_code = response.split('^[0-9]')
-
-    puts response
-    return response[0...-3]
+    response = `curl -X #{GET_METHOD} #{API_URL}#{path}#{params} #{API_KEY_HEADER} #{SILENT_OUTPUT} #{WRITE_STATUS_CODE}`
+    response_status_code = response[-3..-1] # at last 3 characters of response
+    case response_status_code
+    when '503' # request was unavailable - retry immediately
+      make_api_request(path, params)
+    when '403' # rate limit reached - wait 30 seconds before retry
+      sleep(30)
+      make_api_request(path, params)
+    when '200' # OK!
+      return response[0...-3]
+    end
   end
 
   def self.save_to_file(json)
@@ -36,14 +35,14 @@ class MockProcessor
   end
 
   def self.request_all_ids(endpoint)
-    # make a request for the first page
+    # makes a request for the first page
     current_page = 1
     response = make_api_request(endpoint, PAGE_PARAM + current_page.to_s)
     id_hash = JSON.parse(response)
     all_ids_array = id_hash['results']
     total_pages = id_hash['pages']
 
-    # loop through and request all remaining pages
+    # loops through and request all remaining pages
     while current_page < total_pages
       current_page = current_page + 1
       all_ids_array << JSON.parse(make_api_request(endpoint, PAGE_PARAM + current_page.to_s))['results']
@@ -53,6 +52,8 @@ class MockProcessor
   end
 
   save_to_file('{"' + ORGANS_ENDPOINT + '":[' + request_all_ids(ORGANS_ENDPOINT).to_s.delete('[]') + ']},')
+  save_to_file('{"' + USERS_ENDPOINT + '":[' + request_all_ids(USERS_ENDPOINT).to_s.delete('[]') + ']},')
+  save_to_file('{"' + ACCOUNTS_ENDPOINT + '":[' + request_all_ids(ACCOUNTS_ENDPOINT).to_s.delete('[]') + ']},')
 
   @output_file.close
 end
